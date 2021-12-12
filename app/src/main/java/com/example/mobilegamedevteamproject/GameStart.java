cba7c06be6a2b9041c990ad0e3f3fc1f6a905024
Mertt Doğanay <MERTLOL3@outlook.com>
2021-12-11 21:31:49 -0500

    New class GameStart.java created with onCreate method that has contents of mainmenu.xml file

--- app/src/main/java/com/example/mobilegamedevteamproject/GameStart.java
+++ app/src/main/java/com/example/mobilegamedevteamproject/GameStart.java
@@ -12,5 +12,7 @@ public class GameStart extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.mainmenu);
+
+        
     }
 }
