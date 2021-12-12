42ac5caf01d00d59452350898b9f3bd0a31995bb
Mertt Doğanay <MERTLOL3@outlook.com>
2021-12-11 22:04:02 -0500

    Error in MainActivity is fixed about showing the game screen. Dimensions of canvas will be adjusted with using Display

--- app/src/main/java/com/example/mobilegamedevteamproject/MainActivity.java
+++ app/src/main/java/com/example/mobilegamedevteamproject/MainActivity.java
@@ -11,8 +11,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.mainmenu);
-        setContentView(new GameScreen(this));
+        setContentView(R.layout.mainmenu);
+//        setContentView(new GameScreen(this));
     }
 
 
